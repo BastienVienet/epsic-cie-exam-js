9d85c6a89c6e1b594b33b0982a3b4df402ad62b9
BastienVienet <bastien.vienet@outlook.com>
2023-01-11 10:53:24 +0100

    ex1

--- ex1/Partie_3.js
+++ ex1/Partie_3.js
@@ -1,10 +1,10 @@
 let nb = 1;
-let nb2 = 2;
+const nb2 = 2;
 
-document.write("Valeur de nb: " + nb + "<br>");
-document.write("Valeur de nb++: " + nb++ + "<br>");
-document.write("Valeur de nb: " + nb + "<br>");
-document.write("Valeur de nb++: " + ++nb+ "<br>");
-document.write("Valeur de --nb: " + --nb + "<br>");
-nb += nb2;
-document.write("nb+=nb2, valeur de nb:" + nb + "<br>");
+document.write("Valeur de nb: " + nb + "<br>" +
+    "Valeur de nb++: " + nb++ + "<br>" +
+    "Valeur de nb: " + nb + "<br>" +
+    "Valeur de nb++: " + ++nb + "<br>" +
+    "Valeur de --nb: " + --nb + "<br>" +
+    "nb+=nb2, valeur de nb: " + (nb += nb2) + "<br>"
+);
